c1fec6a9b9c150253062866acbd2f329b8c3984a
Training <Training@200411LTP0649.GAVSTECH.COM>
2020-02-27 02:42:20 -0800

    first commit

---
+++ src/first.java
@@ -0,0 +1,26 @@
+/**
+ * 
+ */
+
+/**
+ * @author Training
+ *
+ */
+public class first {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		int a = 1;
+		int b=2;
+		int c = a+b;
+		String name=new String();
+		name="krish";
+		
+		System.out.println(name + "age is 22");
+		System.out.println(c);
+	}
+
+}
+   
